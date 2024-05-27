55d63e4bc9af34c2b482058ba8a4414fad4cee5b
Rick Chen <rickchen19970829@outlook.com>
2024-05-26 17:26:10 -0700

    add get

--- blackhole/common/clients/aws_client.py
+++ blackhole/common/clients/aws_client.py
@@ -8,7 +8,23 @@ class AwsClient:
                 self._get_dynamodb_client()
 
     def _get_dynamodb_client(self):
-        self.dynamodb_client = boto3.client("dynamodb")
+        self.dynamodb_client = boto3.resource("dynamodb")
 
     def store_data_to_dynamodb(self, table_name, data):
         self.dynamodb_client.put_item(TableName=table_name, Item=data)
+
+    def get_data_from_dynamodb(self, table_name, key):
+        if not self.dynamodb_client:
+            self._get_dynamodb_client()
+
+        table = self.dynamodb_client.Table(table_name)
+        response = table.get_item(Key={"primary_key_name": key})
+        return response["Item"]
+
+    def get_all_data_from_dynamodb(self, table_name):
+        if not self.dynamodb_client:
+            self._get_dynamodb_client()
+
+        table = self.dynamodb_client.Table(table_name)
+        response = table.scan()
+        return response["Items"]
--- blackhole/dataprocessor/blueprints/infrastructure_texts_blueprint.py
+++ blackhole/dataprocessor/blueprints/infrastructure_texts_blueprint.py
@@ -1,8 +1,11 @@
 from flask import Blueprint
 from flask_restx import Api, Resource, fields
+from blackhole.common.clients.aws_client import AwsClient
 
 infrastructure_texts_blueprint = Blueprint("infrastructure-texts", __name__)
 api = Api(infrastructure_texts_blueprint)
+
+infra_dynamodb = AwsClient(resources=["dynamodb"])
 
 model = api.model(
     "InfrastructureTextsModel",
@@ -20,7 +23,8 @@ class InfrastructureTextsBlueprint(Resource):
     @api.doc(description="Get infrastructure texts data")
     def get(self):
         # Process the GET request data
-        return {"message": "GET request received"}
+        response = infra_dynamodb.get_all_data_from_dynamodb("infrastructure_texts")
+        return {"message": response}
 
     @api.expect(model)
     @api.doc(description="Store infrastructure texts data")
