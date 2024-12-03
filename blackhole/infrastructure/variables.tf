652845e35b1ea3563a59073fa0b08add65faaf63
Rick Chen <rickchen19970829@outlook.com>
2024-12-02 22:59:05 -0800

    infra

---
+++ blackhole/infrastructure/variables.tf
@@ -0,0 +1,56 @@
+# variables.tf
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
+
+variable "project_name" {
+  description = "Project name for resource naming"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name tag of the VPC to use"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Name tag of the subnet to use"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "root_volume_size" {
+  description = "Size of the root volume in GB"
+  type        = number
+  default     = 50
+}
+
+variable "assign_public_ip" {
+  description = "Whether to assign a public IP to the instance"
+  type        = bool
+  default     = false
+}
+
+variable "allowed_ssh_cidr_blocks" {
+  description = "List of CIDR blocks allowed to SSH to the instance"
+  type        = list(string)
+  default     = []
+}
