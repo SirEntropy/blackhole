8d862a4f8e4ca44d38e90bf96f6e26ec86a7beb9
SirEntropy <47307024+SirEntropy@users.noreply.github.com>
2024-12-17 19:27:29 -0800

    Update variables.tf

--- blackhole/infrastructure/variables.tf
+++ blackhole/infrastructure/variables.tf
@@ -1,56 +1,82 @@
-# variables.tf
 variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
+  description = "The AWS region to create resources in"
   default     = "us-west-2"
 }
 
+variable "org_base_name" {
+  default = "cased"
+}
+
+variable "project_base_name" {
+  default = "comet"
+}
+
+variable "db_name" {
+  description = "Name of the RDS instance"
+  default     = "staging_comet"
+}
+
+variable "db_engine_version" {
+  description = "Version of the database engine"
+  type        = string
+  default     = "16.3" 
+}
+
 variable "environment" {
-  description = "Environment name"
-  type        = string
-  default     = "production"
+  description = "Environment (e.g., staging, production)"
+  default     = "staging"
 }
 
-variable "project_name" {
-  description = "Project name for resource naming"
-  type        = string
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
 }
 
-variable "vpc_name" {
-  description = "Name tag of the VPC to use"
-  type        = string
+variable "az_count" {
+  description = "Number of AZs to use"
+  default     = 2
 }
 
-variable "subnet_name" {
-  description = "Name tag of the subnet to use"
-  type        = string
+variable "app_port" {
+  description = "Port on which the app runs"
+  default     = 8000
 }
 
-variable "ami_id" {
-  description = "AMI ID to use for the EC2 instance"
-  type        = string
+variable "app_count" {
+  description = "Number of Docker containers to run"
+  default     = 1
 }
 
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.medium"
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision"
+  default     = "2048"
 }
 
-variable "root_volume_size" {
-  description = "Size of the root volume in GB"
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "12288"
+}
+
+variable "db_instance_class" {
+  description = "Instance class for the RDS instance"
+  type        = string
+  default     = "db.serverless"
+}
+
+variable "db_allocated_storage" {
+  description = "Allocated storage for the RDS instance in gigabytes"
   type        = number
-  default     = 50
+  default     = 200
 }
 
-variable "assign_public_ip" {
-  description = "Whether to assign a public IP to the instance"
+variable "db_multi_az" {
+  description = "Whether to create a multi-AZ RDS instance"
   type        = bool
   default     = false
 }
 
-variable "allowed_ssh_cidr_blocks" {
-  description = "List of CIDR blocks allowed to SSH to the instance"
-  type        = list(string)
-  default     = []
+variable "db_instance_count" {
+  description = "Number of Aurora DB instances"
+  type        = number
+  default     = 1
 }
