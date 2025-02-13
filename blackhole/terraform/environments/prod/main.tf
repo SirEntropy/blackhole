12691a49c22ee17a12234d6d35db23d3d37c71f2
Rick Chen <rickchen19970829@outlook.com>
2025-02-12 20:15:28 -0800

    new structure

--- blackhole/terraform/environments/prod/main.tf
+++ blackhole/terraform/environments/prod/main.tf
@@ -38,7 +38,7 @@ resource "aws_db_instance" "database" {
   storage_encrypted = true
   
   deletion_protection = true
-  skip_final_snapshot = false
+  skip_final_snapshot = true
 
   tags = {
     Name = "${var.environment}-database"
