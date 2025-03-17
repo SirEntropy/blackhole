36bb72abd466b9926cd0f7235ae4399cebf861b6
SirEntropy <47307024+SirEntropy@users.noreply.github.com>
2025-03-17 11:55:43 -0700

    Update main.tf

--- blackhole/terraform/environments/staging/main.tf
+++ blackhole/terraform/environments/staging/main.tf
@@ -42,17 +42,17 @@ resource "random_string" "redis_username" {
   special = false
 }
 
-resource "random_string" "redis_password" {
+resource "random_string" "redis_username" {
   length  = 16
   special = false
 }
 
-resource "random_string" "db_username" {
+resource "random_string" "redis_username" {
   length  = 16
   special = false
 }
 
-resource "random_string" "db_password" {
+resource "random_string" "redis_username" {
   length  = 16
   special = false
 }
